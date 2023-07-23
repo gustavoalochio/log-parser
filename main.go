6df12dc9009ecc1a4c8a9218525156c4b9997b96
Gustavo Alochio <gustavo.alochio@gmail.com>
2023-07-22 21:53:21 -0300

    chore: fixing tree

--- main.go
+++ main.go
@@ -1,46 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	parser "log-parser/parser"
-	"os"
+	"log-parser/engine"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	gameRunning := false
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		switch {
-		case parser.ParserInitGame(line):
-			gameRunning = true
-			//create game
-			//create player <world>
-			fmt.Println("Game is running!")
-		case parser.ParserInterval(line):
-			if gameRunning {
-				gameRunning = false
-				fmt.Println("Game has been finished")
-			}
-		// case parser.ParserClientConnect(line):
-		//create player in game
-		// case parser.ParserClientInfoUpdate(line):
-		//update player in game
-		// case parser.ParserKilled(line):
-		//update deaths to player
-		//update reasons death hash in game
-		default:
-			fmt.Println("something")
-		}
-
-		// create final message, list players and deaths
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("read err:", err)
-	}
+	engine.ParsingLog()
 }
